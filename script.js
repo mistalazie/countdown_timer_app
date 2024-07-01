f0a787f1817251d0ec5d659de2bab3f8c4e3a607
mistalazie <kiransai537@gmail.com>
2024-07-01 14:58:43 +0530

    A simple countdown timer app

---
+++ script.js
@@ -0,0 +1,20 @@
+let countdownTimer = new Date("July 1, 2024 15:00:00").getTime();
+
+let x = setInterval(function(){
+    let now = new Date().getTime();
+
+    let distance = countdownTimer - now
+
+    let days = Math.floor(distance/(1000*60*60*24));
+    let hours = Math.floor((distance%(1000*60*60*24))/(1000*60*60))
+    let minutes = Math.floor((distance%(1000*60*60)/(1000*60)))
+    let seconds = Math.floor((distance%(1000*60)/(1000)))
+
+    document.getElementById("demo").innerHTML = days + "d " + hours + "h "+ minutes + "m " + seconds + "s "
+
+    if (distance < 0) {
+        clearInterval(x)
+        document.getElementById("demo").innerHTML = "EXPIRED"
+    }
+}, 1000)
+
